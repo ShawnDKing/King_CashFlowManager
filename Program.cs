fc447f0688d65ccacb94fc8b6d028e39a5a58619
ShawnDKing <shawnking38061074@gmail.com>
2022-06-15 12:03:38 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace King_CashFlowManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
